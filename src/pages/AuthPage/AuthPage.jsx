a775aa165c16413e852cd70ac242d30ab53ccbba
Asimmons8228 <96853510+Asimmons8228@users.noreply.github.com>
2023-12-16 14:18:07 -0500

    Added Homepage folder, background image, and centered login/signup form

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,13 +6,13 @@ export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
     <main>
-      <h1>AuthPage</h1>
-      <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
+      <h1>LOGO</h1>
       { showSignUp ?
           <SignUpForm setUser={setUser} />
           :
           <LoginForm setUser={setUser} />
-      }
+        }
+        <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Already A User' : 'Sign Up Here'}</button>
     </main>
   );
 }
