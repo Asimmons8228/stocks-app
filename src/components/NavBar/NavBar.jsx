e59d0c97406d5980675b9bd32f36921f16c10eee
Kevintrxn <kevintran12kt@gmail.com>
2023-12-18 15:35:48 -0600

    Fix NavBar syntax

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,45 +8,25 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <>
-      <nav className='min-w-screen p-1 flex justify-between items-center'>
-        <Link to="/" className='text-white font-bold text-xl px-8'>
-          Portfolio-Pro
-        </Link>
-        <ul className='flex gap-2'>
-            {user ? (
-              <>
-                <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/orders">Order History</Link></li>
-                <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/orders/new">New Order</Link></li>   
-                <li className="flex-shrink-0"><span className="block px-4 py-.5 font-bold text-white">Welcome, {user.name}</span></li>
-                <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="" onClick={handleLogOut}>Log Out</Link></li>
-              </>
-            ) : (
-              <>
-                <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/login">Log In</Link></li>
-                <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/signup">Sign Up</Link></li>
-              </>
-            )}
-
-      <nav className='min-w-screen p-1'>
-          <div className='flex text-white float-left gap-2 pt-1'>
-          </div>
-          <ul className='flex float-right pt-1'>
-              {user ? (
-                <>
-                  <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/portfolio">Portfolio</Link></li>
-                  <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/asset/new">New Asset</Link></li>   
-                  <li className="flex-shrink-0"><span className="block px-4 py-.5 font-bold text-white">Welcome, {user.name}</span></li>
-                  <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="" onClick={handleLogOut}>Log Out</Link></li>
-                </>
-              ) : (
-                <>
-                  <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/login">Log In</Link></li>
-                  <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/signup">Sign Up</Link></li>
-                </>
-              )}
-        </ul>
-      </nav>
-    </>
+    <nav className='min-w-screen p-1 flex justify-between items-center'>
+      <Link to="/" className='text-white font-bold text-xl px-8'>
+        Portfolio-Pro
+      </Link>
+      <ul className='flex gap-2'>
+        {user ? (
+          <>
+            <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/portfolio">Portfolio</Link></li>
+            <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/asset/new">New Asset</Link></li>
+            <li className="flex-shrink-0"><span className="block px-4 py-.5 font-bold text-white">Welcome, {user.name}</span></li>
+            <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="" onClick={handleLogOut}>Log Out</Link></li>
+          </>
+        ) : (
+          <>
+            <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/login">Log In</Link></li>
+            <li className="flex-shrink-0"><Link className="block px-4 py-.5 font-bold text-white" to="/signup">Sign Up</Link></li>
+          </>
+        )}
+      </ul>
+    </nav>
   );
 }
